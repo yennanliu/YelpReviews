ec332bbac4791aa102e7eaf9785a58fc75479aa6
yennanliu <f339339@gmail.com>
2019-09-12 17:29:39 +0800

    add sql build tip table

---
+++ db/tip.sql
@@ -0,0 +1,14 @@
+-- Create mysql db for yelp dataset 
+CREATE DATABASE IF NOT EXISTS yelp;
+
+-- Create yelp tip table 
+use yelp; 
+DROP TABLE IF EXISTS tip;
+CREATE TABLE review  (
+     user_id CHAR(50),  
+     business_id CHAR(50),   
+     text  CHAR(100), 
+     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+     compliment_count INT, 
+     PRIMARY KEY (review_id)
+        );
