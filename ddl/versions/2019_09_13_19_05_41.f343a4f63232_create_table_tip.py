c2fd684720e2b6e7a0d1829809c7fd4c139d7c3b
yennanliu <f339339@gmail.com>
2019-09-13 19:40:23 +0800

    add ddl tip table

---
+++ ddl/versions/2019_09_13_19_05_41.f343a4f63232_create_table_tip.py
@@ -0,0 +1,34 @@
+"""create table tip
+
+Revision ID: f343a4f63232
+Revises: 7042637a71f0
+Create Date: 2019-09-13 19:05:41.862805
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f343a4f63232'
+down_revision = '7042637a71f0'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+    'tip',
+    sa.Column('tip_id', sa.String(100),primary_key=True),
+    sa.Column('user_id', sa.String(100)),
+    sa.Column('business_id', sa.String(100)),
+    sa.Column('text', sa.dialects.mysql.LONGTEXT()),
+    sa.Column('date', sa.TIMESTAMP(), autoincrement=False, nullable=True),
+    sa.Column('compliment_count', sa.Integer()),
+    sa.PrimaryKeyConstraint('tip_id') 
+    )
+
+
+
+def downgrade():
+    op.drop_table('tip')
