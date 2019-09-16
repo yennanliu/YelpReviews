0d24edca270fa5031f8e19a6973373d90df116b8
yennanliu <f339339@gmail.com>
2019-09-16 14:54:34 +0800

    refactor csv dump to mysql script and fix syntax error

--- script/csv_2_mysql.py
+++ script/csv_2_mysql.py
@@ -1,5 +1,9 @@
+import sys
+sys.path.append("./script/")
 import pandas as pd 
 import pymysql
+# UDF 
+from utility import * 
 
 
 def get_conn(mysql_config):
@@ -7,10 +11,10 @@ def get_conn(mysql_config):
     Connect to the database
     """
     # will replace with mysql_config soon 
-    connection = pymysql.connect(host='localhost',
-                 user='user',
-                 password='passwd',
-                 db='db',
+    connection = pymysql.connect(host=mysql_config['host'],
+                 user=mysql_config['user'],
+                 password=mysql_config['password'],
+                 db=mysql_config['dbname'],
                  charset='utf8mb4',
                  cursorclass=pymysql.cursors.DictCursor)
     return connection
@@ -21,13 +25,23 @@ def insert_to_table(df,tablename,connection):
     """
     cols = "`,`".join([str(i) for i in df.columns.tolist()])
     for i,row in df.iterrows():
-    try:
-        with connection.cursor() as cursor:
-        sql = "INSERT INTO tablename (`" +cols + "`) VALUES (" + "%s,"*(len(row)-1) + "%s)"
-        cursor.execute(sql, tuple(row))
-        connection.commit()
-    except Exception as e:
-        print (e, 'insert failed')
-    finally:
-        connection.close()
+        try:
+            with connection.cursor() as cursor:
+                sql = "INSERT INTO {} (`" +cols + "`) VALUES (" + "%s,"*(len(row)-1) + "%s)"
+                sql = sql.format(tablename)
+                #print (sql)
+                print ('insert ok')
+                cursor.execute(sql, tuple(row))
+                connection.commit()
+        except Exception as e:
+            print (e, 'insert failed')
+    connection.close()
+    cursor.close()
 
+
+# mysql_config = parse_config('config/mysql.config')
+# print (mysql_config)
+# conn = get_conn(mysql_config)
+# df = pd.read_csv('data/business_sample.csv')
+# del df['Unnamed: 0']
+# insert_to_table(df.head(10),'business2', conn )
