beb7b1eb90f5f898228a92d047c436dc391aa73a
yennanliu <f339339@gmail.com>
2019-09-16 18:43:04 +0800

    change to dump review data with review sample data

--- script/all_csv_2_mysql.sh
+++ script/all_csv_2_mysql.sh
@@ -4,6 +4,8 @@
 #################################################################
 python script/csv_2_mysql.py data/yelp_academic_dataset_business.csv  business
 python script/csv_2_mysql.py data/yelp_academic_dataset_checkin.csv  checkin
-python script/csv_2_mysql.py data/yelp_academic_dataset_review.csv  review 
+#python script/csv_2_mysql.py data/yelp_academic_dataset_review.csv  review
+# only dump review sample (1 M sub set), since it is a ~ 4G csv 
+python script/csv_2_mysql.py data/yelp_academic_dataset_review_sample.csv  review  
 python script/csv_2_mysql.py data/yelp_academic_dataset_tip.csv  tip
 python script/csv_2_mysql.py data/yelp_academic_dataset_user.csv  user
