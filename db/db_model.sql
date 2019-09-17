c2b6cd218f76e5caa38cd7f71cc357993e153d89
yennanliu <f339339@gmail.com>
2019-09-17 19:41:29 +0800

    add sql that can output db model layout design

---
+++ db/db_model.sql
@@ -0,0 +1,71 @@
+//// -- LEVEL 1
+//// -- Tables and References
+//// https://dbdiagram.io/d
+
+TABLE business  as B {
+     business_id  CHAR [ref: > R.business_id] 
+     name   LONGTEX 
+     address CHAR
+     city CHAR
+     state CHAR
+     postal_code  CHAR
+     latitude  CHAR 
+     longitude CHAR
+     stars INT
+     review_count INT 
+     is_open BOOLEAN
+     attributes LONGTEXT
+     categories LONGTEXT
+     hours LONGTEXT
+        }
+        
+TABLE checkin  as C {
+     user_id varchar 
+     business_id varchar  [ref: > R.business_id] 
+     date  timestamp
+        }
+         
+TABLE tip  as T {
+     user_id varchar [ref: > R.user_id] 
+     business_id varchar  
+     text  varchar
+     date timestamp
+     compliment_count int 
+        }
+      
+TABLE user  as USER {
+     user_id varchar [ref: > R.user_id] 
+     name   varchar
+     review_count INT
+     yelping_since TIMESTAMP 
+     useful INT
+     funny INT  
+     cool  INT  
+     elite varchar 
+     friends varchar
+     fans INT  
+     average_stars FLOAT  
+     compliment_hot  INT 
+     compliment_more  INT 
+     compliment_profile INT 
+     compliment_cute  INT  
+     compliment_list  INT 
+     compliment_note  INT  
+     compliment_plain INT  
+     compliment_cool  INT  
+     compliment_funny INT   
+     compliment_writer INT  
+     compliment_photos INT  
+        }
+  
+TABLE review  as R {
+     review_id varchar
+     user_id varchar
+     business_id varchar
+     stars INT 
+     useful INT 
+     funny INT
+     cool  INT
+     text  varchar 
+     date timestamp
+     }
