0f158d277d3ca92d5f6f9436fee463eda640895b
yennanliu <f339339@gmail.com>
2019-09-17 16:08:07 +0800

    add etl script get user friend count

---
+++ etl/etl_user_friend_count.py
@@ -0,0 +1,34 @@
+import sys
+sys.path.append("./utility/")
+import os
+import pyspark
+import math 
+from datetime import datetime
+from pyspark.sql import SQLContext, Row
+from pyspark import SparkContext
+from pyspark.sql import functions as F
+from pyspark.sql.session import SparkSession
+
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages com.amazonaws:aws-java-sdk-pom:1.7.4,org.apache.hadoop:hadoop-aws:2.7.6 pyspark-shell'
+sc = SparkContext.getOrCreate()
+sqlContext = pyspark.sql.SQLContext(sc)
+spark = SparkSession(sc)
+
+
+if __name__ == '__main__':
+    filename = "data/yelp_academic_dataset_user.json"
+    DF = spark.read.json(filename)
+    DF.printSchema()
+    DF.createOrReplaceTempView("user")
+    # SQL statements can be run by using the sql methods provided by spark
+    userDF = spark.sql("SELECT * from user limit 100")
+    #userDF.show()
+    user_rdd = userDF.rdd 
+    """
+    get how many friends each user has 
+    """
+    friend_rdd = user_rdd.map(lambda x : [ x['user_id'], x['friends']] )\
+                         .map(lambda x : [x[0], len(x[1].split(','))])
+
+    print (friend_rdd.take(30))
+
